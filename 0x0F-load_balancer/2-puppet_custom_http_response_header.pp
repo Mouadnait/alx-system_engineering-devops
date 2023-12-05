f0d0cbd2ac64342ae4b7c18d79b619180abb29a5
mouadnait <moadnait6@gmail.com>
2023-12-05 02:27:09 +0100

    2. Add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,31 @@
+# Automate the task of creating a custom HTTP header response with Puppet.
+# The name of the custom HTTP header must be X-Served-By.
+# The value of the custom HTTP header must be the hostname of the server Nginx is running on.
+
+exec { 'update system':
+    command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://github.com/Mouadnait permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
