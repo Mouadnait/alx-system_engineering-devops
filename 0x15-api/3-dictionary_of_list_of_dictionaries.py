7e2b7d81b909db2a40e8a8a7122f984eb198ab13
mouadnait <moadnait6@gmail.com>
2024-01-04 23:55:18 +0100

    3. Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-"""python script to fetch Rest API for todo lists of employees"""
-
+"""
+python script to fetch Rest API for todo lists of employees
+"""
 import json
 import requests
 import sys
 
 
-api_url = "https://jsonplaceholder.typicode.com/users"
-
-
 if __name__ == '__main__':
-    resp = requests.get(api_url)
+    url = "https://jsonplaceholder.typicode.com/users"
+    resp = requests.get(url)
     Users = resp.json()
     users_dict = {}
     for user in Users:
         USER_ID = user.get('id')
         USERNAME = user.get('username')
-        api_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(USER_ID)
-        api_url = api_url + '/todos/'
-        resp = requests.get(api_url)
+        url = '{}/{}'.format(url, USER_ID)
+        url = url + '/todos/'
+        resp = requests.get(url)
         tasks = resp.json()
         users_dict[USER_ID] = []
         for task in tasks:
@@ -30,6 +28,5 @@ if __name__ == '__main__':
                 "completed": TASK_COMPLETED_STATUS,
                 "username": USERNAME
             })
-            """A little Something"""
     with open('todo_all_employees.json', 'w') as f:
         json.dump(users_dict, f)
