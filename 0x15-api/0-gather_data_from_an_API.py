86661ad10daf9c5d64e84e9fbaf3c88bdd5d0477
mouadnait <moadnait6@gmail.com>
2024-01-05 00:35:39 +0100

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,37 +5,27 @@ Usage: ./0-gather_data_from_an_API.py 2
 where 2 is the user id of Ervin Howell.
 Fake data from "https://jsonplaceholder.typicode.com"
 """
+import re
 import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    # URL for the REST API
-    api_url = f'https://jsonplaceholder.typicode.com'
-    # Make a GET request to the API
-    users = requests.get(api_url + "/users", params={"id": sys.argv[1]})
-    # Check if the request was successful (status code 200)
-    names = users.json()
-    # Extract employee name
-    employee_name = names[0].get('name')
-    # Extract User ID
-    user_id = names[0].get('id')
-    # Count the number of completed tasks
-    users_todo = requests.get(api_url + "/todos", params={"userId": user_id})
-    todos = users_todo.json()
-    completed_tasks = [todo for todo in todos if todo['completed']]
-    num_completed_tasks = len(completed_tasks)
-    total_tasks = len(todos)
-    # Display employee information
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, num_completed_tasks, total_tasks))
-    # Display titles of completed tasks
+api_url = "https://jsonplaceholder.typicode.com"
+
+
+if __name__ == '__main__':
+    id = int(sys.argv[1])
+    req = requests.get('{}/users/{}'.format(api_url, id)).json()
+    task_req = requests.get('{}/todos'.format(api_url)).json()
+    emp_name = req.get('name')
+    tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+    completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+    print(
+        'Employee {} is done with tasks({}/{}):'.format(
+            emp_name,
+            len(completed_tasks),
+            len(tasks)
+        )
+    )
     for task in completed_tasks:
-        print(f"\t{task['title']}")
-
-
-if __name__ == "__main__":
-    # Get employee ID from command-line arguments
-    employee_id = int(sys.argv[1])
-    # Call the function to get and display employee TODO list progress
-    get_employee_todo_progress(employee_id)
+        print('\t {}'.format(task.get('title')))
