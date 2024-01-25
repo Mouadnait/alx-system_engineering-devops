76482bd4f9e59b33e2558e225cbffb178051969e
mouadnait <moadnait6@gmail.com>
2024-01-25 13:28:38 +0100

    1. User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,10 @@
 # changes the OS configuration so that it is posible to login with the holberton user and open a file without any error message
-
 exec { 'change soft limit':
-    command  => "sed -i 's/^holberton hard nofile.*/holberton hard nofile 4096' /etc/security/limits.conf",
+    command  => 'sudo sed -i "s/holberton\ssoft.*/holberton\tsoft\tnofile\t10000/" /etc/security/limits.conf',
     provider => shell,
 }
 
 exec { 'change hard limit':
-    command  => "sed -i 's/^holberton soft nofile.*/holberton soft nofile 4096' /etc/security/limits.conf",
+    command  => 'sudo sed -i "s/holberton\shard.*/holberton\thard\tnofile\t100000/" /etc/security/limits.conf',
     provider => shell,
 }
