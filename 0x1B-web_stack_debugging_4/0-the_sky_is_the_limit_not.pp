ae30d63590fcdf5b0abf5059b18c89da83165333
mouadnait <moadnait6@gmail.com>
2024-01-25 13:40:15 +0100

    0. Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # fix nginx to accept and serve more requests
 exec {'modify max open files limit setting':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  command  => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
   provider => shell,
 }
